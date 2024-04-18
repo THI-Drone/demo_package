616f816d7ab87d94e86659d63a3b1bb90ebc80f5
Niklas <nip4721@thi.de>
2024-04-18 13:08:17 +0000

    fix formatting

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,19 @@
 class MinimalPublisher : public common_lib::CommonNode
 {
 public:
-    /**
-     * @brief Constructs a new MinimalPublisher object.
-     *
-     * @param id The unique name for the node.
-     */
-    MinimalPublisher(std::string id) : CommonNode(id)
-    {
-    }
+  /**
+   * @brief Constructs a new MinimalPublisher object.
+   *
+   * @param id The unique name for the node.
+   */
+  MinimalPublisher(char * id)
+  : CommonNode(id) {}
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MinimalPublisher>(argv[1]));
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>(argv[1]));
+  rclcpp::shutdown();
+  return 0;
 }
