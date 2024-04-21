11f371b530902230b68499e4050b257273ca10ff
Niklas <nip4721@thi.de>
2024-04-21 18:12:20 +0000

    fix: autoformat

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,17 @@
 /**
  * @brief A class that represents a minimal node.
  */
-class MinimalPublisher : public common_lib::CommonNode
-{
-public:
+class MinimalPublisher : public common_lib::CommonNode {
+   public:
     /**
      * @brief Constructs a new MinimalPublisher object.
      *
      * @param id The unique name for the node.
      */
-    MinimalPublisher(std::string id) : CommonNode(id)
-    {
-    }
+    MinimalPublisher(std::string id) : CommonNode(id) {}
 };
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MinimalPublisher>(argv[1]));
     rclcpp::shutdown();
